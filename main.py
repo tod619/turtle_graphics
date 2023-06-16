5aec0d8396d998356f3c67484293ba819807e386
tod619 <trevorodwyer619@hotmail.com>
2023-06-16 21:46:12 +0100

    Draw a dashed line with turtle in main.py file

--- main.py
+++ main.py
@@ -8,9 +8,16 @@ tim = Turtle()
 # timmy_the_turtle.color("red")
 
 # Draw a square
-for i in range(4):
-    tim.forward(100)
-    tim.left(90)
+# for i in range(4):
+#     tim.forward(100)
+#     tim.left(90)
+
+# Draw a dashed line
+for i in range(15):
+    tim.forward(10)
+    tim.penup()
+    tim.forward(10)
+    tim.pendown()
 
 screen = Screen()
 screen.exitonclick()
