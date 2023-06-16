705eb46c021e728d7042a68a151e03aecc2a58ab
tod619 <trevorodwyer619@hotmail.com>
2023-06-16 21:32:07 +0100

    Refactor code and shorten variable name to tim in main.py file

--- main.py
+++ main.py
@@ -3,13 +3,14 @@
 
 from turtle import Turtle, Screen
 
-timmy_the_turtle = Turtle()
+tim = Turtle()
 # timmy_the_turtle.shape("turtle")
 # timmy_the_turtle.color("red")
 
+# Draw a square
 for i in range(4):
-    timmy_the_turtle.forward(100)
-    timmy_the_turtle.left(90)
+    tim.forward(100)
+    tim.left(90)
 
 screen = Screen()
 screen.exitonclick()
