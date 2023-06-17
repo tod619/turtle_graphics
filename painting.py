503bee7f469ea0d0e36c9412cd2ea3c04d1b4054
tod619 <trevorodwyer619@hotmail.com>
2023-06-17 19:48:49 +0100

    Import turtle module and test in painting.py file

--- painting.py
+++ painting.py
@@ -1,13 +1,22 @@
-import colorgram
+# import colorgram
+import turtle as turtle_module
+import random
 
-rgb_colors = []
-colors = colorgram.extract("image.jpg", 30)
+tim3 = turtle_module.Turtle()
 
-for color in colors:
-    r = color.rgb.r
-    g = color.rgb.g
-    b = color.rgb.b
-    new_color = (r, g, b)
-    rgb_colors.append(new_color)
+# rgb_colors = []
+# colors = colorgram.extract("image.jpg", 30)
 
-print(rgb_colors)
+# for color in colors:
+#     r = color.rgb.r
+#     g = color.rgb.g
+#     b = color.rgb.b
+#     new_color = (r, g, b)
+#     rgb_colors.append(new_color)
+
+# print(rgb_colors)
+color_list = [(139, 168, 195), (206, 154, 121), (192, 140, 150), (25, 36, 55), (58, 105, 140), (145, 178, 162), (151, 68, 58), (137, 68, 76), (229, 212, 107), (47, 36, 41), (145, 29, 36), (28, 53, 47), (55, 108, 89),
+              (228, 167, 173), (189, 99, 107), (139, 33, 28), (194, 92, 79), (49, 40, 36), (228, 173, 166), (20, 92, 69), (177, 189, 212), (29, 62, 107), (113, 123, 155), (172, 202, 190), (51, 149, 193), (166, 200, 213)]
+
+screen = turtle_module.Screen()
+screen.exitonclick()
