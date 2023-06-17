a29f7340f39d71239c649a626e49b8587fdb1a2d
tod619 <trevorodwyer619@hotmail.com>
2023-06-17 21:21:29 +0100

    Fix bugs in code in painting.py file

--- painting.py
+++ painting.py
@@ -4,6 +4,7 @@ import random
 
 turtle_module.colormode(255)
 tim3 = turtle_module.Turtle()
+tim3.speed("fastest")
 
 # rgb_colors = []
 # colors = colorgram.extract("image.jpg", 30)
@@ -20,11 +21,12 @@ color_list = [(139, 168, 195), (206, 154, 121), (192, 140, 150), (25, 36, 55), (
               (228, 167, 173), (189, 99, 107), (139, 33, 28), (194, 92, 79), (49, 40, 36), (228, 173, 166), (20, 92, 69), (177, 189, 212), (29, 62, 107), (113, 123, 155), (172, 202, 190), (51, 149, 193), (166, 200, 213)]
 
 tim3.penup()
+tim3.hideturtle()
 tim3.setheading(225)
 tim3.forward(300)
 tim3.setheading(0)
 
-number_of_dots = 100
+number_of_dots = 101
 
 
 for dot_count in range(1, number_of_dots):
